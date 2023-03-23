720706314593df3072eb0e9fdb3260516ac90ab5
jakeengstrom3 <jakeengstrom3@gmail.com>
2023-03-23 12:43:10 -0700

    every frame in the camera stream is no longer classfifed. Press SPACE to classify the current frame.

--- classify.py
+++ classify.py
@@ -33,7 +33,7 @@ _FONT_THICKNESS = 1
 _FPS_AVERAGE_FRAME_COUNT = 10
 
 # Bin Parameters
-_UNLOCK_THRESHOLD = 0.8
+_UNLOCK_THRESHOLD = 0.6
 
 
 def run(model: str, max_results: int, score_threshold: float, num_threads: int,
@@ -82,37 +82,42 @@ def run(model: str, max_results: int, score_threshold: float, num_threads: int,
           'ERROR: Unable to read from webcam. Please verify your webcam settings.'
       )
 
-    counter += 1
-    image = cv2.flip(image, 1)
+    # Only classify the image when spacebar is pressed
+    if cv2.waitKey(1) == 32: # Spacebar code
+      counter += 1
+      image = cv2.flip(image, 1)
+      # Convert the image from BGR to RGB as required by the TFLite model.
+      rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Convert the image from BGR to RGB as required by the TFLite model.
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      # Create TensorImage from the RGB image
+      tensor_image = vision.TensorImage.create_from_array(rgb_image)
+      # List classification results
+      categories = classifier.classify(tensor_image)
 
-    # Create TensorImage from the RGB image
-    tensor_image = vision.TensorImage.create_from_array(rgb_image)
-    # List classification results
-    categories = classifier.classify(tensor_image)
+      # Show classification results on the image
+      for idx, category in enumerate(categories.classifications[0].categories):
+        category_name = category.category_name
+        score = round(category.score, 2)
 
-    # Show classification results on the image
-    for idx, category in enumerate(categories.classifications[0].categories):
-      category_name = category.category_name
-      score = round(category.score, 2)
+        result_text = category_name + ' (' + str(score) + ')'
+        text_location = (_LEFT_MARGIN, (idx + 2) * _ROW_SIZE)
+        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+            _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
 
-      if("nonRecyclable" not in category_name and score > _UNLOCK_THRESHOLD):
-        cap.release()
-        cap = cv2.VideoCapture(camera_id)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        if(save_images_on):
-          write_out_image_to_classified_directory(image, category.category_name)
-        print("UNLOCKED")
-        time.sleep(4)
-        print("LOCKED")
+        if("nonRecyclable" not in category_name and score > _UNLOCK_THRESHOLD):
+          cap.release()
+          cap = cv2.VideoCapture(camera_id)
+          cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+          cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+          if(save_images_on):
+            write_out_image_to_classified_directory(image, category.category_name)
+          print("UNLOCKED")
+          time.sleep(4)
+          print("LOCKED")
+        else:
+          print("NOT RECYCLEABLE. If this is incorrect, press the challenge button (c)")
 
-      result_text = category_name + ' (' + str(score) + ')'
-      text_location = (_LEFT_MARGIN, (idx + 2) * _ROW_SIZE)
-      cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-          _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
+
 
     # Calculate the FPS
     if counter % _FPS_AVERAGE_FRAME_COUNT == 0:
@@ -129,6 +134,7 @@ def run(model: str, max_results: int, score_threshold: float, num_threads: int,
     # Stop the program if the ESC key is pressed.
     if cv2.waitKey(1) == 27:
       break
+
     cv2.imshow('image_classification', image)
 
   cap.release()

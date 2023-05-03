5d0f63c074afb3b62acf29d748c96d69cbf876b9
jakeengstrom3 <jakeengstrom3@gmail.com>
2023-05-03 10:51:51 -0700

    possible fix for importing model-maker

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 
 # Install Python dependencies.
 python3 -m pip install pip --upgrade
-python3 -m pip install -r mmrequirements.txt
 python3 -m pip install -r requirements.txt
+pythno3 -m pip install tflite-model-maker

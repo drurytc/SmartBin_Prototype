6f59e4e8b3945fc963d28a8fa1a77d7d568de4a4
alsalmaf <56139553+alsalmaf@users.noreply.github.com>
2023-05-22 23:49:19 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@
 # Install Python dependencies.
 python3 -m pip install pip --upgrade
 python3 -m pip install -r requirements.txt --upgrade
+
+
+#LED dependencies
+sudo apt-get install python3-dev python3-rpi.gpio
+pip install RPi.GPIO
